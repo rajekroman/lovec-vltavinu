dd4b4d75aefd9b9737a1f690eb01b18c7c3ca8a2
rajekroman <108610159+rajekroman@users.noreply.github.com>
2026-07-21 19:12:24 +0200

    Exportovat data, stav a objektivy modulárního jádra

--- src/index.js
+++ src/index.js
@@ -15,3 +15,25 @@ export {
 } from "./systems/CollisionSystem.js";
 export { AnimationSystem, createAnimation } from "./systems/AnimationSystem.js";
 export { HybridRenderer } from "./render/HybridRenderer.js";
+export { LEVELS, LEVEL_BY_ID, getLevel, requireLevel } from "./data/levels.js";
+export { PERKS, PERK_BY_ID, getPerk, requirePerk, createEmptyPerks, normalizePerks } from "./data/perks.js";
+export { SAMPLES, SAMPLE_BY_ID, getSample, requireSample } from "./data/samples.js";
+export {
+  GAME_RUNTIME_VERSION,
+  LEGACY_STATE_VERSION,
+  SAVE_SCHEMA_VERSION,
+  DEFAULT_STATS,
+  normalizeStats,
+  createGameState,
+  cloneGameState,
+  validateGameState
+} from "./state/GameState.js";
+export {
+  CURRENT_SAVE_KEY,
+  LEGACY_SAVE_KEYS,
+  parseLegacySave,
+  serializeLegacySave,
+  readLegacySave,
+  migrateLegacySave
+} from "./adapters/LegacySaveAdapter.js";
+export { evaluateObjective, isObjectiveComplete } from "./gameplay/Objectives.js";
