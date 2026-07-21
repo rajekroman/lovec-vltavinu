86d016b058848b9326ee2bb71f932233dee0e3d4
rajekroman <108610159+rajekroman@users.noreply.github.com>
2026-07-21 19:18:08 +0200

    Exportovat GameState, save adaptér a objektivy

--- src/index.js
+++ src/index.js
@@ -24,3 +24,24 @@ export {
 export { PERK_DEFINITIONS, getPerkDefinition } from "./data/perks.js";
 export { SAMPLE_DEFINITIONS, getSampleDefinition } from "./data/samples.js";
 export { validateGameData, assertValidGameData } from "./data/validateGameData.js";
+export {
+  GAME_RUNTIME_VERSION,
+  LEGACY_STATE_VERSION,
+  SAVE_SCHEMA_VERSION,
+  DEFAULT_STATS,
+  createEmptyPerks,
+  normalizePerks,
+  normalizeStats,
+  createGameState,
+  cloneGameState,
+  validateGameState
+} from "./state/GameState.js";
+export {
+  CURRENT_SAVE_KEY,
+  LEGACY_SAVE_KEYS,
+  parseLegacySave,
+  serializeLegacySave,
+  readLegacySave,
+  migrateLegacySave
+} from "./adapters/LegacySaveAdapter.js";
+export { evaluateObjective, isObjectiveComplete } from "./gameplay/Objectives.js";
