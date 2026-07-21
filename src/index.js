a6835f9c88cb3a0eefa44fe838443b89205420df
rajekroman <108610159+rajekroman@users.noreply.github.com>
2026-07-21 19:10:56 +0200

    Export structured game data registry

--- src/index.js
+++ src/index.js
@@ -15,3 +15,13 @@ export {
 } from "./systems/CollisionSystem.js";
 export { AnimationSystem, createAnimation } from "./systems/AnimationSystem.js";
 export { HybridRenderer } from "./render/HybridRenderer.js";
+export {
+  LEVEL_DEFINITIONS,
+  LEVEL_ORDER,
+  LEVEL_BY_ID,
+  getLevelDefinition,
+  getNextLevelId
+} from "./data/levels.js";
+export { PERK_DEFINITIONS, PERK_BY_ID, getPerkDefinition } from "./data/perks.js";
+export { SAMPLE_DEFINITIONS, SAMPLE_BY_ID, getSampleDefinition } from "./data/samples.js";
+export { validateGameData, assertValidGameData } from "./data/validateGameData.js";
