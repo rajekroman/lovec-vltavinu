6c86c195e09210e7432076921d75e50b0fb129c6
rajekroman <108610159+rajekroman@users.noreply.github.com>
2026-07-22 12:39:55 +0200

    feat(architecture): export modular bootstrap contracts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { EventBus } from "./core/EventBus.js";
+export { EVENT_CONTRACTS, GAME_EVENT_NAMES, validateEventPayload } from "./core/GameEvents.js";
 export { GameLoop } from "./core/GameLoop.js";
 export { SceneManager } from "./core/SceneManager.js";
 export { InputManager } from "./core/InputManager.js";
@@ -15,12 +16,20 @@ export {
 } from "./systems/CollisionSystem.js";
 export { AnimationSystem, createAnimation } from "./systems/AnimationSystem.js";
 export { HybridRenderer } from "./render/HybridRenderer.js";
+export { ThreeRenderer } from "./render/ThreeRenderer.js";
+export { DomInputAdapter } from "./input/DomInputAdapter.js";
+export { ScreenController } from "./ui/ScreenController.js";
+export { HudController } from "./ui/HudController.js";
+export { TitleScene } from "./scenes/TitleScene.js";
+export { ChlumScene } from "./scenes/ChlumScene.js";
 export {
   LEVEL_DEFINITIONS,
   LEVEL_ORDER,
   getLevelDefinition,
   getNextLevelId
 } from "./data/levels.js";
+export { GameSession, createGameSession } from "./gameplay/GameSession.js";
+export { evaluateObjective, isObjectiveComplete } from "./gameplay/Objectives.js";
 export { PERK_DEFINITIONS, getPerkDefinition } from "./data/perks.js";
 export { SAMPLE_DEFINITIONS, getSampleDefinition } from "./data/samples.js";
 export { validateGameData, assertValidGameData } from "./data/validateGameData.js";
