c4d1a1118dd8d9e30beeaef33324bc3b2d9c97a0
rajekroman <108610159+rajekroman@users.noreply.github.com>
2026-07-23 22:51:02 +0200

    Doplnit Slavia assety do offline cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lovec-vltavinu-slavia-v6-0";
+const CACHE = "lovec-vltavinu-slavia-v6-1";
 const CORE = [
   "./", "./index.html", "./style.css", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png",
   "./vendor/three.module.min.js", "./vendor/three.core.min.js", "./src/bootstrap.js",
@@ -9,11 +9,11 @@ const CORE = [
   "./src/render/HybridRenderer.js", "./src/render/ThreeRenderer.js", "./src/render/GltfAssetLoader.js", "./src/render/AssetDisposal.js", "./src/render/ModelFactory.js",
   "./vendor/three/addons/loaders/GLTFLoader.js", "./vendor/three/addons/utils/BufferGeometryUtils.js", "./vendor/three/addons/utils/SkeletonUtils.js",
   "./src/input/DomInputAdapter.js", "./src/ui/ScreenController.js", "./src/ui/HudController.js", "./src/scenes/TitleScene.js", "./src/scenes/ChlumScene.js", "./src/scenes/ChlumNesmenBridgeScene.js", "./src/scenes/NesmenScene.js", "./src/scenes/NesmenRestorationScene.js", "./src/scenes/NesmenBesedniceBridgeScene.js", "./src/scenes/BesedniceScene.js",
-  "./assets/manifests/assets.json", "./assets/sprites/player/hunter-walk-sheet.png", "./assets/sprites/npcs/farmer-vaclav.png", "./assets/sprites/npcs/forester-jan.png", "./assets/sprites/npcs/expert-eva.svg", "./assets/sprites/npcs/rival-franta.svg",
+  "./assets/manifests/assets.json", "./assets/sprites/player/hunter-walk-sheet.png", "./assets/sprites/npcs/farmer-vaclav.png", "./assets/sprites/npcs/forester-jan.png", "./assets/sprites/npcs/expert-eva.svg", "./assets/sprites/npcs/rival-franta.svg", "./assets/sprites/npcs/expert-eva.png", "./assets/sprites/npcs/thief-franta.png",
   "./assets/sprites/findings/vltavin-common.png", "./assets/sprites/findings/vltavin-rare.png", "./assets/sprites/findings/vltavin-standard.png", "./assets/sprites/findings/vltavin-nesmen.png",
   "./assets/textures/terrain/chlum-field.png", "./assets/textures/terrain/chlum-furrows.png", "./assets/textures/terrain/nesmen-forest-floor.png", "./assets/textures/terrain/nesmen-sand-profile.png", "./assets/textures/slavia/kd-slavia-facade.svg", "./assets/textures/slavia/event-banner.svg",
   "./assets/models/chlum/tractor-no-driver.glb", "./assets/models/chlum/hay-bale.glb", "./assets/models/chlum/field-marker.glb", "./assets/models/chlum/field-fence-segment.glb",
-  "./assets/models/nesmen/profile-marker.glb", "./assets/models/nesmen/tree-stump.glb", "./assets/models/slavia/kd-slavia.gltf"
+  "./assets/models/nesmen/profile-marker.glb", "./assets/models/nesmen/tree-stump.glb", "./assets/models/slavia/kd-slavia.gltf", "./assets/models/slavia/kd-slavia.glb", "./assets/models/slavia/document-folder.glb"
 ];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE))); self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))); self.clients.claim(); });
